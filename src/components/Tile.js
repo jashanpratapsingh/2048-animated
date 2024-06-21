efc191497060c23795f019940904a33524bb05da
jashanpratapsingh <jashanpratap123@gmail.com>
2024-06-21 14:58:28 +0400

    Correcting the TIle function

--- src/components/Tile.js
+++ src/components/Tile.js
@@ -1,27 +1,30 @@
-import React from "react";
+import React from 'react'
 
-const Tile = ({tile}) => {
-    let classArray = ['tile'];
-    classArray.push('tile'+tile.value);
-    if(!tile.mergedInto) {
-        classArray.push(`position_${tile.row}_${tile.column}`);
-    }
+export default function Tile({tile}) {
+  let classArray = ['tile'];
+  classArray.push(`tile${tile.value}`);
 
-    if(tile.mergedInto) {
-        classArray.push(`merged`);
-    }
-    if(tile.isNew()){
-        classArray.push(`new`);
-    }
-    if(tile.hasMoved()) {
-        classArray.push(`row_from_${tile.fromRow()}_to_${tile.toRow()}`);
-        classArray.push(`column_from_${tile.fromColumn()}_to_${tile.toColumn()}`);
-        classArray.push(`isMoving`);
-    }
+  if(!tile.mergeInto) {
+    classArray.push(`position_${tile.row}_${tile.column}`);
+  }
 
-    let classes = classArray.join(" ");
+  if(tile.mergeInto) {
+    classArray.push('merged');
+  }
 
-    return <div className="tile">Tile</div>
-};
+  if(tile.isNew()) {
+    classArray.push('new');
+  }
 
-export default Tile;
+  if(tile.hasMoved()) {
+    classArray.push(`row_from_${tile.fromRow()}_to_${tile.toRow()}`);
+    classArray.push(`column_from_${tile.fromColumn()}_to_${tile.toColumn()}`);
+    classArray.push('isMoving');
+  }
+
+  let classes = classArray.join(' ');
+
+  return (
+    <span className={classes}></span>
+  )
+}
